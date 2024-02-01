8557f372d7c6f6c26c7cefaab381023cf36c04e1
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2024-02-01 17:05:52 +0100

    add: projects from api

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,21 @@
-<script >
-export default {
-  components: {
-    
-    
-  },
-  data() {
-    return {
-      
-    }
-  },
-  mounted() {
-
-  },
-  methods: {
-       
-    },
-  }
-
-</script>
-
 <template>
- 
+  <div id="app">    
+    <ProjectList />
+  </div>
 </template>
 
-<style lang="scss">
+<script>
+import ProjectList from './components/ProjectList.vue';
+
+export default {
+  components: {
+    ProjectList,
+  },
+};
+</script>
+
+<style lang='scss'>
 @use "./style/general.scss";
 @import "@fortawesome/fontawesome-free/css/all.css"; 
+
 </style>
---
+++ src/components/ProjectList.vue
@@ -0,0 +1,41 @@
+<template>
+
+
+  <div>
+      <h2>Lista dei Progetti</h2>
+      <ul>
+        <li v-for="project in projects.projects" :key="project.id">
+          {{ project.title }}
+        </li>
+      </ul>
+    </div>
+</template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        projects: [],
+      };
+    },
+    mounted() {
+      this.fetchProjects();
+    },
+    methods: {
+      fetchProjects() {
+        fetch('http://127.0.0.1:8000/api/projects')
+          .then(response => response.json())
+          .then(data => {
+            this.projects = data;
+            console.log(this.projects);
+          })
+          .catch(error => console.error('Errore nella richiesta API:', error));
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+
+  </style>
+  
