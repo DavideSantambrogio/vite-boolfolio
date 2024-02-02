bd84dc9ba7e9add214c25f04cdc5b503a7e27cd1
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2024-02-02 16:09:31 +0100

    fix files names

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,11 @@
+<template>
+
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+/* Stile specifico del componente se necessario */
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,12 @@
+<template>
+
+  </template>
+  
+  <script>
+
+  </script>
+  
+  <style scoped>
+  /* Stile specifico del componente se necessario */
+  </style>
+  
